e7d4ce5076aa54944feffac7da22c8db893d38b3
Cengiz YILMAZ <terabilgi@gmail.com>
2023-06-18 23:43:51 +0300

    product layout oluşturuldu

---
+++ app/products/page.tsx
@@ -0,0 +1,30 @@
+
+
+const Product = () => {
+  return (
+    <>
+        <table className="table w-full">
+            <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Produt Name</td>
+                    <td>Price</td>
+                    <td>Brand</td>
+                    <td className="text-center">Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </>
+  )
+}
+
+export default Product
